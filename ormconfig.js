7584b75e92aa0186f95d0920bf7784ba53bf0ab7
Mohammed Aminu <aminuolawalekan@gmail.com>
2021-04-11 12:04:25 +0100

    updated readme

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,6 @@
 module.exports = {
   type: 'postgres',
-  url:
-    'postgres://poclsmzgibgomn:07a516e869c3d647d699b6ce95d6b1109e46f345b1af5c9df319818c6f4e2c87@ec2-34-225-103-117.compute-1.amazonaws.com:5432/dapdqjhrc5pplm?ssl=true',
+  url: process.env.DATABASE_URL,
   entities: [process.env.ENTITY_PATH],
   synchronize: true,
 };
